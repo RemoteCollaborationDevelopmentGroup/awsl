2aaf521c5f5d1341639cc3aa74e94eaf6dfec98a
Last <huan0016@gmail.com>
2020-11-18 05:22:40 +0800

    add go mod

---
+++ server/go.mod
@@ -0,0 +1,5 @@
+module awsl
+
+go 1.14
+
+require github.com/lucas-clemente/quic-go v0.19.1
---
+++ server/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/lucas-clemente/quic-go/http3"
+	"net/http"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("23333------------")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("hello quic"))
+	})
+	http3.ListenAndServe("0.0.0.0:443", "./ssl/4790070_satori.love.pem", "./ssl/4790070_satori.love.key", nil)
+}
