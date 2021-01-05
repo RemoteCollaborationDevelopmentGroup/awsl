ed4aa407c7de7e627a2539363803e10e9a2d2fe1
Last <huan0016@gmail.com>
2021-01-05 14:35:07 +0800

    port 8000

---
+++ components/TaskItem.vue
@@ -0,0 +1,46 @@
+<style>
+.task {
+  padding: 0.5em;
+  background: #ff9999;
+  margin: 5px;
+  border-radius: 5px;
+}
+</style>
+
+<template lang="pug">
+.task(@click="click")
+  span.on X
+  span {{ name }}
+  span {{ content }}
+  span 
+</template>
+
+<script>
+// 组件中应当只有交互
+// 没有对数据的 API 操作
+export default {
+  props: ["data"], // data一般只传递 id
+  data: () => ({
+    name: "任务名称",
+    content: "主体信息详情和资源的引用",
+    partner: [121],
+    status: "", // 开放 | 执行 | 暂停 | 终止 | 结束 | 完成
+  }),
+  methods: {
+    hover() {
+      // hover 时预装载数据
+    },
+    click() {
+      // click 时展开项, 但列表中的其它项目应能快速切换
+      this.name = "new task name";
+      this.content = "0";
+    },
+    // 关注任务/参与任务
+    // 任务独占/开放协作
+    // 任务进度/任务完成
+    // 更新任务
+    // 回复 / 会话 ? IM信息
+    // 节点信息 / 标签信息 / 源project / 发布者
+  },
+};
+</script>
--- pages/remote.vue
+++ pages/remote.vue
@@ -1,25 +1,35 @@
 <template lang="pug">
-div.container_remote
+.container_remote
   div
     div
-      img.NuxtImg(src="/emmm.jpg" alt="awsl" style="height:10rem;")
+      img.NuxtImg(src="/emmm.jpg", alt="awsl", style="height: 10rem")
       p 施工中..
     div
       span 软件 网络 交互 产品设计 服务
     div
-      Task(v-for="i in 8" :key="i") {{ i }}
+      TaskItem(v-for="id in tasklist", :key="id", :data="id")
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuesaxLogo from '~/components/VuesaxLogo.vue'
+import Logo from "~/components/Logo.vue";
+import VuesaxLogo from "~/components/VuesaxLogo.vue";
 
 export default {
+  data: () => ({
+    tasklist: [1, 2, 3, 4, 5, 6],
+    //[
+    //  { id: 1, name: "一条任务信息", info: "其它参数信息" },
+    //  { id: 5, name: "一条任务信息", info: "其它参数信息" },
+    //  { id: 4, name: "一条任务信息", info: "其它参数信息" },
+    //  { id: 3, name: "一条任务信息", info: "其它参数信息" },
+    //  { id: 2, name: "一条任务信息", info: "其它参数信息" },
+    //],
+  }),
   components: {
     Logo,
-    VuesaxLogo
-  }
-}
+    VuesaxLogo,
+  },
+};
 </script>
 
 <style lang="styl">
